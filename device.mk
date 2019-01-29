28a4fc61c63852ada22e1f8ba51e65b025571c61
Vachounet <vachounet@live.fr>
2019-01-29 17:34:56 +0100

    potter: switch to SnapdragonCamera2
    Change-Id: I660f6d64a9ae00c822e41b05d17b769cd4ae7572

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Camera2 \
+    SnapdragonCamera2 \
     libgui_vendor \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
